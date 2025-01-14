c919546217615e943d32977ca39078199a01bc2c
t-silvers <thomas.silvers@me.com>
2025-01-14 18:24:08 +0100

    update vcf clean models

--- models/vcfs/manifest.sql
+++ models/vcfs/manifest.sql
@@ -1,3 +1,5 @@
 copy (
-    select * from glob('{{ glob }}') where "file" not ilike '%.clean%.parquet' and "file" not ilike '%.filter%.parquet';
+    select * from glob('{{ glob }}')
+    where "file" not ilike '%.clean%.parquet'
+      and "file" not ilike '%.filter%.parquet'
 ) to '{{ output }}' (format csv);
