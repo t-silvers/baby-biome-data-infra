6d4cd80e3b0f7efbcb53b4dd2249a1d206797eef
t-silvers <thomas.silvers@me.com>
2025-01-19 18:06:43 +0100

    squash me

--- models/biospecimens/insert.sql
+++ models/biospecimens/insert.sql
@@ -1,3 +1,39 @@
+create type if not exists relationship as enum (
+    select relationship from read_csv('{{ relationship }}')
+);
+
+create type if not exists species as enum (
+    select species from read_csv('{{ species }}')
+);
+
+create type if not exists timepoint as enum (
+    select timepoint from read_csv('{{ timepoint }}')
+);
+
+create table if not exists samples (
+    sample uinteger,
+    library varchar,
+    id varchar,
+    family varchar[],
+    relationship relationship[],
+    donor varchar[],
+    timepoint varchar[],
+    collection_interval_category timepoint[],
+    collection_interval interval[],
+    species varchar[],
+    species_stdized species[],
+    plate usmallint[],
+    well varchar[],
+    barcode_1 varchar[],
+    barcode_2 varchar[],
+    notes varchar[],
+    fastq_1 varchar[],
+    fastq_2 varchar[],
+    primary key (library, id)
+);
+
+create sequence if not exists "sample";
+
 create temp table new_samples as
 with
     prepared_samplesheet as (
