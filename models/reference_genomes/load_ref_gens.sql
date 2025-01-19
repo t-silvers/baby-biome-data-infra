69c512ecc167360c9db1c75e6ea3361d16e04aa5
t-silvers <thomas.silvers@me.com>
2025-01-20 00:20:21 +0100

    squash me

--- models/reference_genomes/load_ref_gens.sql
+++ models/reference_genomes/load_ref_gens.sql
@@ -1,11 +1,13 @@
 create table if not exists reference_genomes (
     sample uinteger,
-    reference_genome varchar,
+    taxon_plate varchar,
+    taxon_seq varchar,
     new_est_reads uinteger,
     fraction_total_reads float,
+    reference_genome varchar,
     primary key (sample)
 );
 
 insert or ignore into reference_genomes
     by name
-    select * from new_reference_genomes;
+    select * from read_csv('{{ ref_genomes }}');
--- models/reference_genomes/transform_bracken_ref_gens.sql
+++ models/reference_genomes/transform_bracken_ref_gens.sql
@@ -1,11 +1,16 @@
 set preserve_insertion_order = false;
 set max_temp_directory_size = '{{ max_temp_directory_size }}';
 
-attach '{{ annot_sequencing_records }}' (read_only);
+attach '{{ annot_seq_db }}' as seq_records (read_only);
 
 create temp table new_reference_genomes as
 with
-    bracken as (
+    plate_taxa as (
+        select "sample", taxon_plate 
+        from seq_records.annot_sequencing_records
+    ),
+
+    raw_bracken as (
         select "sample"
             , "name"
             , new_est_reads
@@ -13,66 +18,73 @@ with
         from read_parquet('{{ bracken_glob }}', hive_partitioning = false)
     ),
 
-    top_species_id as (
-        select "sample", max(fraction_total_reads) as max_frac
-        from bracken
-        group by "sample"
-    ),
-
-    final as (
+    cleaned_bracken as (
         select * exclude("name")
             , regexp_replace(
                 trim("name"), ' ', '_', 'g'
-            ) as reference_genome
-        from bracken
-        where ("sample", fraction_total_reads) in (
-            select ("sample", max_frac) from top_species_id
+            ) as taxon_seq
+        from raw_bracken
+    ),
+
+    top_species_by_reads_frac as (
+        select * exclude(ranked_taxa)
+        from (
+            select *
+                , row_number() over (
+                    partition by "sample" order by fraction_total_reads desc
+                ) ranked_taxa
+            from cleaned_bracken
         )
+        where ranked_taxa = 1
+    ),
+
+    joined as (
+        select t1.*, t2.* exclude("sample")
+        from plate_taxa t1
+        left join top_species_by_reads_frac t2
+            on t1.sample = t2.sample
+    ),
+
+    -- Filter based on sequencing support
+    bracken_read_filtered as (
+        select "sample" from joined
+        where new_est_reads > pow(10, cast('{{ read_pow }}' as int))
+          and fraction_total_reads > cast('{{ read_frac }}' as float)
+    ),
+    
+    -- Check that plate-based genus agrees w seq-based
+    agreement_filtered as (
+        select "sample" from joined
+        where (
+            split_part(taxon_plate, '_', 1) = split_part(taxon_seq, '_', 1)
+            or taxon_plate is null
+        )
+    ),
+
+    -- Reference must be available
+    available_reference_filtered as (
+        select "sample" from joined
+        where list_contains(
+            string_split('{{ available_genomes }}', '|'), taxon_seq
+        )
+    ),
+
+    reference_genomes as (
+        select "sample", taxon_seq, taxon_seq as reference_genome
+        from joined
+        where "sample" in (select "sample" from bracken_read_filtered)
+          and "sample" in (select "sample" from agreement_filtered)
+          and "sample" in (select "sample" from available_reference_filtered)
+    ),
+
+    final as (
+        select t1.*, t2.reference_genome
+        from joined t1
+        left join reference_genomes t2
+            on t1.sample = t2.sample
+           and t1.taxon_seq = t2.taxon_seq
     )
+
 select * from final;
 
-
-copy (
-    with
-        samplesheet as (
-            select * from read_csv('{{ sample_info }}')
-        ),
-
-        joined as (
-            select t1.*, t2.* exclude("sample")
-            from samplesheet t1
-            left join reference_genomes t2
-                on t1.sample = t2.sample
-               and t2.reference_genome is not null
-        ),
-
-        -- Filter based on support
-        bracken_read_filtered as (
-            select "sample" from joined
-            where new_est_reads > pow(10, cast('{{ read_pow }}' as int))
-              and fraction_total_reads > cast('{{ read_frac }}' as float)
-        ),
-
-        -- Check that plate-based genus agrees w seq-based
-        agreement_filtered as (
-            select "sample" from joined
-            where split_part(species_stdized, '_', 1) = split_part(reference_genome, '_', 1)
-        ),
-
-        -- Reference must be available
-        available_reference_filtered as (
-            select "sample" from joined
-            where list_contains(
-                string_split('{{ available_genomes }}', '|'),
-                reference_genome
-            )
-        ),
-
-        final as (
-            select * from joined
-            where "sample" in (select "sample" from bracken_read_filtered)
-              and "sample" in (select "sample" from agreement_filtered)
-              and "sample" in (select "sample" from available_reference_filtered)
-        )
-    select * from final
-) to '{{ samplesheet }}' (format csv);
+copy (select * from new_reference_genomes) to '{{ ref_genomes }}' (format csv);
